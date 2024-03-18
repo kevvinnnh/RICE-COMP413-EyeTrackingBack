a355f18c08258c10b35bbd27bc0fa00e8acf0173
Sophia Rebello <sir5@rice.edu>
2024-03-17 20:21:57 -0500

    Survey data goes into database successfully

--- app2.py
+++ app2.py
@@ -7,6 +7,7 @@ import random
 import datetime
 import string
 import bcrypt
+import requests
 
 
 app = Flask(__name__)
@@ -19,6 +20,7 @@ uri = "mongodb+srv://Project5-Sophia:koch@project5.bpvocwv.mongodb.net/?retryWri
 client = MongoClient(uri)
 
 # Set up the database
+#TODO: This will be shared by ALL users!! Either make sure code is concurrent safe or make sure the database is 100% concurrent safe through testing 
 db = client.get_database("EpiDerm")
 
 # this is for testing
@@ -44,7 +46,9 @@ def register_user():
             return jsonify({"error": "Invalid email or role"}), 400
 
         # Check if the email is already registered
-        if users_collection.find_one({"email": email}):
+        participant_collection = db.get_collection("Participants")
+        creator_collection = db.get_collection("Creators")
+        if participant_collection.find_one({"email": email}) or creator_collection.find_one({"email": email}):
             return jsonify({"error": "Email already registered"}), 400
 
         # Generate a random password
@@ -53,18 +57,19 @@ def register_user():
         # Hash the password
         hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
+        #TODO: Get rid of user_id because mongoDB generates a unique _id that will replace user_id
         # Generate a unique user ID
         user_id = ''.join(random.choices(string.ascii_letters + string.digits, k=10))
 
         # Store user data in the respective collection based on role
         if role == "participant":
-            participants_collection.insert_one({
+            participant_collection.insert_one({
                 "email": email,
                 "userId": user_id,
                 "password": hashed_password
             })
         elif role == "creator":
-            creators_collection.insert_one({
+            creator_collection.insert_one({
                 "email": email,
                 "userId": user_id,
                 "password": hashed_password
@@ -87,6 +92,7 @@ def register_user():
 def create_form():
     try:
         form_data = request.json
+        #TODO: Get rid of formID because mongoDB generates a unique _id that will replace form_id
         # Generate a unique alphanumeric formID
         formID = ''.join(random.choices(string.ascii_letters + string.digits, k=10))
         # Add a dateCreated field with the current date and time
@@ -126,6 +132,29 @@ def get_questions():
     except Exception as e:
         return jsonify({"status": "error", "message": str(e)}), 500
 
+@app.route('/api/forms', methods=['GET'])
+@cross_origin()
+# this returns the questions to the frontend from the database
+def get_forms():
+    url = "https://us-east-2.aws.data.mongodb-api.com/app/data-bpzpl/endpoint/data/v1/action/find"
+    payload = json.dumps({
+        "collection": "Forms",
+        "database": "EpiDerm",
+        "dataSource": "Project5",
+    })
+    headers = {
+      'Content-Type': 'application/json',
+      'Access-Control-Request-Headers': '*',
+      'api-key': 'yJAaJITqsyGpxeQ3gEZIsip2M6o0iWp6jpdddoGJY4WZ2PgPVEaGRqgRjY2SjZJ2',
+    }
+    # try:
+    #     collection = db.get_collection("Questions")
+    #     questions = list(collection.find())
+    response = requests.request("POST", url, headers=headers, data=payload)
+    return jsonify({"status": "success", "forms": forms}), 200
+    # except Exception as e:
+    #     return jsonify({"status": "error", "message": str(e)}), 500
+
 @app.route('/test', methods=['GET'])
 def test_atlas_connection():
     try:
@@ -138,25 +167,48 @@ def test_atlas_connection():
 @app.route('/api/responses', methods=['POST'])
 @cross_origin()
 def receive_responses():
-    try:
-        responses_data = request.json
-        print(responses_data)
-        # Eventually, we can store the default survey in the database & it would act as a created survey
-        answers = ['Benign keratosis-like lesions','Melanocytic nevi','Benign keratosis-like lesions']
-        count=0
-        # Function that grades hard coded default survey skin lesion questions
-        for i in range(len(answers)):
-            index = str(i)
-            if responses_data[index] == answers[i]:
-                count+=1
+    # try:
+    responses_data = request.json
+    print(responses_data)
+    # Eventually, we can store the default survey in the database & it would act as a created survey
+    answers = ['Benign keratosis-like lesions','Melanocytic nevi','Benign keratosis-like lesions']
+    count=0
+    response_dict = {}
+    # Function that grades hard coded default survey skin lesion questions
+    for i in range(len(answers)):
+        index = str(i)
+        response_dict[i] = responses_data[index]
+        if responses_data[index] == answers[i]:
+            count+=1
 
-        # Save the received responses to the database or perform any necessary processing
-        # collection = db.get_collection("Questions")
-        # inserted_ids = collection.insert_many(responses_data).inserted_ids
-
-        return jsonify({"status": "success", "message": "Responses received and stored successfully"}), 201
-    except Exception as e:
-        return jsonify({"status": "error", "message": str(e)}), 500
+    # Save the received responses to the database or perform any necessary processing
+    # collection = db.get_collection("Responses")
+    # print(collection)
+    # result = collection.insert_one({"test": "connection"})
+    # inserted_ids = collection.insert_one({"user_id": 0,"form_id": 0, "response_id": 0, "role": responses_data["role"], "years_of_experience": responses_data["experienceLevel"],
+    #                                         "age": responses_data["age"],"vision_impairment": responses_data["vision"], "gender": responses_data["gender"],"correctness_score": count,
+    #                                         "eye_tracking_data": [], "responses": response_dict})
+    # print(inserted_ids)
+    url = "https://us-east-2.aws.data.mongodb-api.com/app/data-bpzpl/endpoint/data/v1/action/insertOne"
+    payload = json.dumps({
+        "collection": "Responses",
+        "database": "EpiDerm",
+        "dataSource": "Project5",
+        "document": {"user_id": 0,"form_id": 0, "role": responses_data["role"], "years_of_experience": responses_data["experienceLevel"],
+                                            "age": responses_data["age"],"vision_impairment": responses_data["vision"], "gender": responses_data["gender"],"correctness_score": count,
+                                            "eye_tracking_data": [], "responses": response_dict}
+    })
+    headers = {
+      'Content-Type': 'application/json',
+      'Access-Control-Request-Headers': '*',
+      'api-key': 'yJAaJITqsyGpxeQ3gEZIsip2M6o0iWp6jpdddoGJY4WZ2PgPVEaGRqgRjY2SjZJ2',
+    }
+    response = requests.request("POST", url, headers=headers, data=payload)
+    print(response)
+    return jsonify({"status": "success", "message": "Responses received and stored successfully"}), 201
+    # except Exception as e:
+    #     print(e)
+    #     return jsonify({"status": "error", "message": str(e)}), 500
 
 #new route to get all forms' information and contents
 @app.route('/api/forms', methods=['GET'])
--- schemas.py
+++ schemas.py
@@ -1,19 +1,23 @@
-from mongoengine import Document, StringField, ListField, IntField, DateTimeField, ObjectIdField
+from mongoengine import Document, StringField, ListField, IntField, DateTimeField, ObjectIdField, DictField
 
 class Participant(Document):
     email = StringField(required=True)
-    user_id = ObjectIdField()
-    role = StringField()
-    years_of_experience = IntField()
-    age = IntField()
-    vision_impairment = ListField(StringField())
+    #user_id = ObjectIdField()
 
 class Response(Document):
     user_id = ObjectIdField()
     form_id = ObjectIdField()
+    #Enforces that each response is different 
+    #response_id = ObjectIdField()
+    role = StringField()
+    years_of_experience = IntField()
+    age = IntField()
+    gender = StringField()
+    vision_impairment = ListField(StringField())
     correctness_score = IntField()
     # add question to map to its corresponding eye tracking data
     eye_tracking_data = ListField(DictField())
+    responses = ListField(DictField())
 
 class UserForm(Document):
     user_id = ObjectIdField()
@@ -24,10 +28,10 @@ class UserForm(Document):
 class Creator(Document):
     email = StringField(required=True)
     password = StringField(required=True)
-    user_id = ObjectIdField()
+    #user_id = ObjectIdField()
     forms_created = ListField(ObjectIdField())
 
 class Form(Document):
-    form_id = ObjectIdField()
+    #form_id = ObjectIdField()
     responses = ListField(DictField())
     # add correct answers later
